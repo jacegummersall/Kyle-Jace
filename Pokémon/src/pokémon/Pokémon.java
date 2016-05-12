166760c72f78165dcc433a7dbee8491e851dde7f
jacegummersall <jacegummersall@10.18.144.253>
2016-05-12 09:21:20 -0600

    5-10-16 Initial commit of project

--- Pokémon/src/pokémon/Pokémon.java
+++ Pokémon/src/pokémon/Pokémon.java
@@ -5,6 +5,8 @@
  */
 package pokémon;
 
+import byui.cit260.pokemonGame.model.Player;
+
 /**
  *
  * @author jacegummersall
@@ -15,7 +17,13 @@ public class Pokémon {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Player playerOne = new Player();
+        
+        playerOne.setName("Red");
+        playerOne.setBestTime(7.00);
+        
+        String playerInfo = playerOne.toString();
+        System.out.println(playerInfo);
     }
     
 }
