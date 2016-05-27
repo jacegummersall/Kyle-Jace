24c34f669379d52de1f111cd901a5bf94362a7e2
Thunder <Thunder@DESKTOP-QNJVEE5>
2016-05-27 09:55:19 -0600

    Merge origin/master
    Conflicts:
    	Pokémon/src/pokémon/Pokémon.java

--- Pokémon/src/byui/cit260/pokemonGame/control/PotionControl.java
+++ Pokémon/src/byui/cit260/pokemonGame/control/PotionControl.java
@@ -5,10 +5,27 @@
  */
 package byui.cit260.pokemonGame.control;
 
+import java.util.Random;
+
 /**
  *
- * @author jacegummersall
+ * @author kylehathaway
  */
 public class PotionControl {
     
+    public int usePotion(int pokemonCurrentHP, int pokemonFullHP) {
+        if (pokemonCurrentHP >= pokemonFullHP){
+            return -1;
+        }
+        if (pokemonCurrentHP < 1){
+            return -2;
+        }
+        
+        Random random = new Random();
+        
+        int usePotion = pokemonCurrentHP + 20 + random.nextInt(10);
+        
+        return usePotion;
+    
+    }
 }
