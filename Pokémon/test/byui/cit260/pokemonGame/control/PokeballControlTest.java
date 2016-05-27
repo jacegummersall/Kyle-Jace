d067c3da15d2acf7275ea773f8b2161df41f11a8
jacegummersall <jacegummersall@10.18.144.253>
2016-05-27 09:58:04 -0600

    5-10-16 Initial commit of project

---
+++ Pokémon/test/byui/cit260/pokemonGame/control/PokeballControlTest.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.pokemonGame.control;
+
+import java.util.Random;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author jacegummersall
+ */
+public class PokeballControlTest {
+    
+    public PokeballControlTest() {
+    }
+
+    /**
+     * Test of calcPokeball method, of class PokeballControl.
+     */
+    @Test
+    public void testCalcPokeball() {
+        System.out.println("calcPokeball");
+        
+        //Test Case 1
+        
+        System.out.println("/tTest 1");
+        
+        //Input values
+        int pokemonCurrentHP = 35;
+        int pokemonFullHP = 100;
+        
+        //create instance for calcPokeball
+        PokeballControl instance = new PokeballControl();
+        String expResult = "Pokémon Captured!";
+        
+        //call function
+        Random random = new Random();
+        boolean captureSucess = pokemonCurrentHP + ( 1 * random.nextInt(5)) < pokemonFullHP/2;
+        
+        String result = instance.calcPokeball(pokemonCurrentHP, pokemonFullHP);
+        assertEquals(expResult, result);
+       
+         //Test Case 2
+        
+        System.out.println("/tTest 2");
+        
+        //Input values
+        pokemonCurrentHP = 65;
+        pokemonFullHP = 85;
+        
+        //create instance for calcPokeball
+        expResult = "Pokémon has escaped, he is too strong";
+        
+        //call function
+        captureSucess = pokemonCurrentHP + ( 1 * random.nextInt(5)) < pokemonFullHP/2;
+        
+        result = instance.calcPokeball(pokemonCurrentHP, pokemonFullHP);
+        assertEquals(expResult, result);
+        
+          //Test Case 3
+        
+        System.out.println("/tTest 3");
+        
+        //Input values
+        pokemonCurrentHP = 55;
+        pokemonFullHP = -110;
+        
+        //create instance for calcPokeball
+        expResult = "ERROR";
+        
+        //call function
+        captureSucess = pokemonCurrentHP + ( 1 * random.nextInt(5)) < pokemonFullHP/2;
+        
+        result = instance.calcPokeball(pokemonCurrentHP, pokemonFullHP);
+        assertEquals(expResult, result);
+        
+        //Test Case 4
+        
+        System.out.println("/tTest 4");
+        
+        //Input values
+        pokemonCurrentHP = 25;
+        pokemonFullHP = 45;
+        
+        //create instance for calcPokeball
+        expResult = "Pokémon has escaped, he is too strong";
+        
+        //call function
+        captureSucess = pokemonCurrentHP + ( 1 * random.nextInt(5)) < pokemonFullHP/2;
+        
+        result = instance.calcPokeball(pokemonCurrentHP, pokemonFullHP);
+        assertEquals(expResult, result);
+        
+       //Test Case 5
+        
+        System.out.println("/tTest 5");
+        
+        //Input values
+        pokemonCurrentHP = -5;
+        pokemonFullHP = 50;
+        
+        //create instance for calcPokeball
+        expResult = "Pokémon has fainted";
+        
+        //call function
+        captureSucess = pokemonCurrentHP + ( 1 * random.nextInt(5)) < pokemonFullHP/2;
+        
+        result = instance.calcPokeball(pokemonCurrentHP, pokemonFullHP);
+        assertEquals(expResult, result); 
+        
+
+    }
+    
+}
