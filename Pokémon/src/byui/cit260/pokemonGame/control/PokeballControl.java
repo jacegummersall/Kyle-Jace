a5c3380202d5f127f696cfbb0483079dc7998ef8
jacegummersall <jacegummersall@10.18.144.253>
2016-05-27 09:30:21 -0600

    5-10-16 Initial commit of project

--- Pokémon/src/byui/cit260/pokemonGame/control/PokeballControl.java
+++ Pokémon/src/byui/cit260/pokemonGame/control/PokeballControl.java
@@ -5,10 +5,43 @@
  */
 package byui.cit260.pokemonGame.control;
 
+import java.util.Random;
+
 /**
  *
  * @author jacegummersall
  */
 public class PokeballControl {
-    
+
+    public String calcPokeball(int pokemonCurrentHP, int pokemonFullHP) {
+
+        if (pokemonCurrentHP > 98) {
+            return "Pokémon has escaped, he is too strong";
+        }
+
+        if (pokemonCurrentHP < 1) {
+            return "Pokémon has fainted";
+        }
+
+        if (pokemonFullHP < 0) {
+            return "ERROR";
+        }
+        
+        if (pokemonFullHP < 50) {
+            return "Pokémon has escaped, he is too strong";
+        }
+
+        if (pokemonFullHP > 200) {
+            return "ERROR";
+        }
+
+        Random random = new Random();
+
+        int captureSuccess = pokemonCurrentHP + (1 * random.nextInt(5));
+        if (captureSuccess < pokemonFullHP / 2) {
+            return "Pokémon Captured!";
+        }
+        return "Pokémon has escaped, he is too strong";
+    }
+
 }
